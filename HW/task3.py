026ec521764100539689c56ab1febaa08478074c
Lyudmila <l.xmail@yandex.ru>
2022-09-22 19:23:01 +0300

    Task3 completed

--- HW/task3.py
+++ HW/task3.py
@@ -1,2 +1,20 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 
+data_input = open('HW\input.txt', 'r')
+data_output = open('HW\output.txt', 'w')
+string_input = data_input.read()
+string_output = ''
+element = string_input[0]
+count = 1
+for i in range(1, len(string_input)):
+    if element == string_input[i]:
+        count+= 1
+    else:
+        string_output = string_output + str(count) + str (element)
+        count = 1
+        element = string_input[i]
+    
+string_output = string_output + str(count) + str (element)
+data_output.write(string_output)
+data_input.close
+data_output.close
